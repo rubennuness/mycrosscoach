f919d8d376db9f0382d8ee55691ddfd04fe23030
rubennuness <rubennunes12@gmail.com>
2025-05-06 05:25:30 -0400

    railway test

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
+// server/db.js
 const mysql = require('mysql2/promise');
 
+// Railway cria automaticamente MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQLDATABASE, MYSQLPORT
+const {
+  MYSQLHOST,
+  MYSQLUSER,
+  MYSQLPASSWORD,
+  MYSQLDATABASE,
+  MYSQLPORT
+} = process.env;
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'coach_user',    // ou root (não recomendado em prod)
-  password: '123',
-  database: 'coachdb'
+  host    : MYSQLHOST,
+  user    : MYSQLUSER,
+  password: MYSQLPASSWORD,
+  database: MYSQLDATABASE,
+  port    : MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit  : 10
 });
 
 module.exports = pool;
-
