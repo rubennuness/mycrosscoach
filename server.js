d3d67eb93b5b4f1fa1e1ee29de26d838aed7e58f
rubennunes <rubennunes12@gmail.com>
2025-05-28 16:23:23 +0100

    .

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const userRoutes = require('./routes/users');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('./uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Rotas de autenticação
 app.use('/auth', authRoutes);
